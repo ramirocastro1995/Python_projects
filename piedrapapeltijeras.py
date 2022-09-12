e0fd0c0024910bb205b8cfd89d8268750ae2131d
Ramiro Castro <53920544+ramirocastro1995@users.noreply.github.com>
2022-09-12 08:57:08 -0300

    Update piedrapapeltijeras.py

--- piedrapapeltijeras.py
+++ piedrapapeltijeras.py
@@ -1,22 +1,59 @@
 import random, sys
 
+
 perdidas = 0
 ganadas = 0
 empatadas = 0
 
-while True:
-    print('Piedra papel o tijera')
+def main():
     while True:
-        print('%s Ganadas, %s Perdidas, %s empatadas' % (ganadas,perdidas,empatadas))
-        movimientoJugador = input()
-        print('ingresa tu movimiento (Pi)edra,(Pa)pel,(Ti)jera o (S)alir')
-        if movimientoJugador == 'S':
-            sys.exit()
-        if movimientoJugador == 'PI':
-            print('piedra contra...')
-        elif movimientoJugador == 'PA':
-            print('papel contra')
-        elif movimientoJugador == 'TI':
+        print('Piedra papel o tijera')
+        movimientoJugador = eligeMovimientoJugador()
+        movimientoMaquina = eligeMovimientoMaquina()
+        jugada = definirJugada(movimientoJugador, movimientoMaquina)
+        if jugada == ('Ganador'):
+            ganadas = ganadas +1
+
+
+
+
+def eligeMovimientoJugador():
+    movimientoJugador = input()
+    if movimientoJugador == 'PI':
+        print('piedra contra...')
+    elif movimientoJugador == 'PA':
+        print('papel contra')
+    elif movimientoJugador == 'TI':
             print('Tijeras contra..')
-        
-        
+    return movimientoJugador
+
+def eligeMovimientoMaquina():
+    valor = ''
+    movimientoMaquina = random.randint(1,3)
+    if movimientoMaquina == 1:
+        valor == 'TI'
+        print('Tijera')
+    elif movimientoMaquina == 2:
+        valor == 'PA'
+        print('Papel')
+    elif movimientoMaquina == 3:
+        valor == 'PI'
+        print('Piedra')
+    return valor
+    
+def definirJugada(movimientoJugador,movimientoMaquina):
+    if movimientoJugador == movimientoMaquina :
+        print('Empate!')
+    elif movimientoJugador == 'PA' and movimientoMaquina == 'PI':
+        print('Ganaste!')
+        return('Ganador')
+    elif movimientoJugador == 'PI' and movimientoMaquina == 'PA':
+        print('Perdiste!')
+        return('Perdedor')
+
+
+
+
+
+
+main()
